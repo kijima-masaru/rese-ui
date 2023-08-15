685d52c923bc6078661d26882ff8c725723d8d75
kijima masaru <kijima4510@gmail.com>
2023-08-15 13:42:02 +0900

    お気に入り登録機能の実装完了・そのためのUserモデルの修正

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany; //お気に入り登録機能のために追加
 
 class User extends Authenticatable
 {
