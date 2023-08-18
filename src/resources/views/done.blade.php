558597d8a28af1d47a727a8ab21517d63e35b706
kijima masaru <kijima4510@gmail.com>
2023-08-18 15:56:29 +0900

    予約完了ページのcss調整

--- src/resources/views/done.blade.php
+++ src/resources/views/done.blade.php
@@ -7,48 +7,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>予約完了ページ</title>
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/done.css.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/done.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
 </head>
 
 <body>
     <main>
-        <div class="done">
-            <!-- 共通ヘッダー -->
-            <div class="common">
-                <div class="header">
-                    <h1>Rese</h1>
-                </div>
-                <div class="header__right">
-                    <div class="header__content">
-                        <div class="header__url">
-                            <a href="{{ route('mypage') }}">マイページ</a>
-                        </div>
-                        <div class="header__url">
-                            <a href="{{ route('shops.index') }}">店舗一覧</a>
-                        </div>
-                        <div class="header__logout">
-                            <form class="logout__form" action="/logout" method="post">
-                                @csrf
-                                <button class="logout__button">ログアウト</button>
-                            </form>
-                        </div>
+        <!-- 共通ヘッダー -->
+        <div class="common">
+            <div class="header">
+                <h1>Rese</h1>
+            </div>
+            <div class="header__right">
+                <div class="header__content">
+                    <div class="header__url">
+                        <a href="{{ route('mypage') }}">マイページ</a>
+                    </div>
+                    <div class="header__url">
+                        <a href="{{ route('shops.index') }}">店舗一覧</a>
+                    </div>
+                    <div class="header__logout">
+                        <form class="logout__form" action="/logout" method="post">
+                            @csrf
+                            <button class="logout__button">ログアウト</button>
+                        </form>
                     </div>
                 </div>
             </div>
-
-            <!-- ページ本体 -->
-            <div class="done">
-                <div class="done__text">
-                    <h1>ご予約ありがとうございます</h1>
-                </div>
-                <div class="done__content">
-                    @if(session('reservation'))
-                        <p>日付: {{ session('reservation')['day'] }}</p>
-                        <p>時間: {{ session('reservation')['time'] }}</p>
-                        <p>人数: {{ session('reservation')['people'] }}</p>
-                    @endif
-                </div>
+        </div>
+        <!-- ページ本体 -->
+        <div class="done">
+            <div class="done__head">
+                <h1>ご予約ありがとうございます！！</h1>
+            </div>
+            <div class="done__content">
+                @if(session('reservation'))
+                    <p>日付: {{ session('reservation')['day'] }}</p>
+                    <p>時間: {{ session('reservation')['time'] }}</p>
+                    <p>人数: {{ session('reservation')['people'] }}</p>
+                @endif
+            </div>
+            <div class="done__text">
+                <p>予約内容の確認・変更はマイページから行えます。</p>
             </div>
         </div>
     </main>
