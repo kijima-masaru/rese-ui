7d42aacc9ae7302b0b676e50b3827d4aeaafe952
kijima masaru <kijima4510@gmail.com>
2023-08-20 22:36:25 +0900

    管理者・店舗代表者・利用者で表示するページを切り替えるための準備

---
+++ src/resources/views/admin.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>管理者ページ</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/admin.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
+</head>
+
+<body>
+    <main>
+        <!-- 共通ヘッダー -->
+        <div class="common">
+            <div class="header">
+                <h1>Rese</h1>
+            </div>
+            <div class="header__right">
+                <div class="header__content">
+                    <div class="header__url">
+                        <a href="{{ route('mypage') }}">マイページ</a>
+                    </div>
+                    <div class="header__url">
+                        <a href="{{ route('shops.index') }}">店舗一覧</a>
+                    </div>
+                    <div class="header__logout">
+                        <form class="logout__form" action="/logout" method="post">
+                            @csrf
+                            <button class="logout__button">ログアウト</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ページ本体 -->
+        <div class="admin"></div>
+    </main>
+</body>
