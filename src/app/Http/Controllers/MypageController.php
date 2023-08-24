fb19adcfab25a39fe52ab00125cd491f6ee23d32
kijima masaru <kijima4510@gmail.com>
2023-08-24 14:27:42 +0900

    マイページにおいて、reservesテーブルのstatusカラムの値がafterの場合、予約内容が表示されないように修正

--- src/app/Http/Controllers/MypageController.php
+++ src/app/Http/Controllers/MypageController.php
@@ -12,8 +12,13 @@ class MypageController extends Controller
     public function index()
     {
         $user = Auth::user();
-        $reserves = Reserve::where('user_id', $user->id)->get(); // リレーションを使用して予約データを取得
-        $favoriteShops = auth()->user()->favorites;
+
+        // "after" ステータスの予約を表示しないように、whereNotInを使用して条件を追加します
+        $reserves = Reserve::where('user_id', $user->id)
+            ->whereNotIn('status', ['after'])
+            ->get(); // リレーションを使用して予約データを取得
+
+            $favoriteShops = auth()->user()->favorites;
 
         return view('mypage', compact('reserves', 'favoriteShops'));
     }
