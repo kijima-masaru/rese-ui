b6f8c736aaa6b3e6b34c888872a1354027f66f84
kijima masaru <kijima4510@gmail.com>
2023-09-29 21:09:10 +0900

    当日予約をする場合に現在の時刻より前の時間で予約できないように修正しました。

--- src/resources/views/detail_shop.blade.php
+++ src/resources/views/detail_shop.blade.php
@@ -67,7 +67,7 @@
                     @csrf
                     <div class="reserve__form">
                         <label for="day">日付:</label>
-                        <input type="date" name="day" required><br>
+                        <input type="date" name="day" required min="{{ \Carbon\Carbon::now()->format('Y-m-d') }}"><br>
                         @error('day')
                             <p class="error">{{ $message }}</p>
                         @enderror
@@ -93,5 +93,33 @@
             </div>
         </div>
     </main>
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            // 日付と時間の入力要素を取得
+            var dateInput = document.querySelector('input[name="day"]');
+            var timeInput = document.querySelector('input[name="time"]');
+
+            // 現在の日付と時間を取得
+            var now = new Date();
+            var currentYear = now.getFullYear();
+            var currentMonth = (now.getMonth() + 1).toString().padStart(2, '0');
+            var currentDay = now.getDate().toString().padStart(2, '0');
+            var currentHour = now.getHours().toString().padStart(2, '0');
+            var currentMinute = now.getMinutes().toString().padStart(2, '0');
+
+            // 最小日付を今日の日付に設定
+            dateInput.min = currentYear + '-' + currentMonth + '-' + currentDay;
+
+            // 日付が今日の場合、時間の最小値を現在の時間に設定
+            dateInput.addEventListener('change', function () {
+                if (dateInput.value === (currentYear + '-' + currentMonth + '-' + currentDay)) {
+                timeInput.min = currentHour + ':' + currentMinute;
+                } else {
+                    // 日付が変更された場合、時間の最小値をクリア
+                    timeInput.min = '';
+                }
+            });
+        });
+    </script>
 </body>
 </html>
