8d7c63b53fa17709ccfdcefa0112815ad16be189
kijima masaru <kijima4510@gmail.com>
2023-10-04 20:59:28 +0900

    detail_shop.blade.php内のJavaScriptをdetail_shop.jsに移動

--- src/resources/views/detail_shop.blade.php
+++ src/resources/views/detail_shop.blade.php
@@ -138,33 +138,6 @@
             @endif
         </div>
     </main>
-    <script>
-        document.addEventListener("DOMContentLoaded", function () {
-            // 日付と時間の入力要素を取得
-            var dateInput = document.querySelector('input[name="day"]');
-            var timeInput = document.querySelector('input[name="time"]');
-
-            // 現在の日付と時間を取得
-            var now = new Date();
-            var currentYear = now.getFullYear();
-            var currentMonth = (now.getMonth() + 1).toString().padStart(2, '0');
-            var currentDay = now.getDate().toString().padStart(2, '0');
-            var currentHour = now.getHours().toString().padStart(2, '0');
-            var currentMinute = now.getMinutes().toString().padStart(2, '0');
-
-            // 最小日付を今日の日付に設定
-            dateInput.min = currentYear + '-' + currentMonth + '-' + currentDay;
-
-            // 日付が今日の場合、時間の最小値を現在の時間に設定
-            dateInput.addEventListener('change', function () {
-                if (dateInput.value === (currentYear + '-' + currentMonth + '-' + currentDay)) {
-                timeInput.min = currentHour + ':' + currentMinute;
-                } else {
-                    // 日付が変更された場合、時間の最小値をクリア
-                    timeInput.min = '';
-                }
-            });
-        });
-    </script>
+    <script src="{{ asset('js/detail_shop.js') }}"></script>
 </body>
 </html>
