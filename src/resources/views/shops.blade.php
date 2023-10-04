0334c9773898b98399ae36673506a2de4574dab2
kijima masaru <kijima4510@gmail.com>
2023-10-04 20:39:31 +0900

    店舗一覧ページにおいて、各店舗の口コミ評価の平均値が表示されるように修正

--- src/resources/views/shops.blade.php
+++ src/resources/views/shops.blade.php
@@ -70,6 +70,12 @@
                         <div class="shop__name">
                             <h2>{{ $shop->name }}</h2>
                         </div>
+                        @php
+                            $averageRating = $shop->reviews ? $shop->reviews->avg('rating') : null;
+                        @endphp
+                        <div class="view__content">
+                            <p>★ {{ $averageRating !== null ? number_format($averageRating, 1) : '評価なし' }}</p>
+                        </div>
                         <div class="shop__view">
                             <div class="view__content">
                                 <p>#{{ $areaDataMap[$shop->id]->area }}</p>
